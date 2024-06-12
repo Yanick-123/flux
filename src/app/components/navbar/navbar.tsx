bedca0127b28bab10792e55faca897e6b2684e68
Yanick Kros <yanick.kros@gmail.com>
2024-06-12 14:15:31 +0200

    Adding content to the NavBar

--- src/app/components/navbar/navbar.tsx
+++ src/app/components/navbar/navbar.tsx
@@ -1,8 +1,37 @@
 import React from 'react'
 import styles from './navbar.module.css'
+import Image from 'next/image'
+import icon1 from '../../../../public/img/icons/Home.svg'
+import icon2 from '../../../../public/img/icons/New-Group.svg'
+import icon3 from '../../../../public/img/icons/Discover.svg'
+import icon4 from '../../../../public/img/icons/Profile.svg'
+import icon1Active from '../../../../public/img/icons/HomeActive.svg'
+import icon2Active from '../../../../public/img/icons/New-GroupActive.svg'
+import icon3Active from '../../../../public/img/icons/DiscoverActive.svg'
+import icon4Active from '../../../../public/img/icons/ProfileActive.svg'
 
 export default function NavBar() {
   return (
-    <div>navbar</div>
+    <>
+        <div className={styles.NavBarContainer}>
+            <div>
+                <Image src={icon1Active} width={25} height={25} alt="" />
+                <p className={styles.TagActive}>Home</p>
+            </div>
+            <div>
+                <Image src={icon2} width={25} height={25} alt="" />
+                <p className={styles.Tag}>New Group</p>
+            </div>
+            <div>
+                <Image src={icon3} width={25} height={25} alt="" />
+                <p className={styles.Tag}>Discover</p>
+            </div>
+            <div>
+                <Image src={icon4} width={25} height={25} alt="" />
+                <p className={styles.Tag}>Profile</p>
+            </div>
+        </div>
+    </>
+
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import NavBar from "./components/navbar/navbar";
 
 export default function Home() {
   return (
    <>
-    <div className="PhoneFrame">
-    </div>
+      <NavBar></NavBar>
    </>
   );
 }
